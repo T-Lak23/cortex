fdb099fc06785cc55867e20b95001b14ef716d2a
T-Lak23 <tilak230412@gmail.com>
2026-09-02 15:26:42 +0530

    fireabse changes in auth

--- backend/services/auth/config/firebase.js
+++ backend/services/auth/config/firebase.js
@@ -1,10 +1,20 @@
-import { cert, initializeApp } from "firebase-admin";
+// import { cert, initializeApp } from "firebase-admin";
+
+// const path = process.env.FIREBASE_KEY_PATH || "./serviceAccountKey.json";
+// // import serviceAccount from "../serviceAccountKey.json" with { type: "json" };
+// const serviceAccount = await import(path, {
+//   assert: { type: "json" }, // Node 18+ supports JSON modules
+// });
+
+// export const app = initializeApp({
+//   credential: cert(serviceAccount),
+// });
+
+import fs from "fs";
+import { cert, initializeApp } from "firebase-admin/app";
 
 const path = process.env.FIREBASE_KEY_PATH || "./serviceAccountKey.json";
-// import serviceAccount from "../serviceAccountKey.json" with { type: "json" };
-const serviceAccount = await import(path, {
-  assert: { type: "json" }, // Node 18+ supports JSON modules
-});
+const serviceAccount = JSON.parse(fs.readFileSync(path, "utf-8"));
 
 export const app = initializeApp({
   credential: cert(serviceAccount),
