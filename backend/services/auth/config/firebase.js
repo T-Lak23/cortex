49140364a37073ed78016dbc2307dad469f97bb1
T-Lak23 <tilak230412@gmail.com>
2026-09-02 13:51:36 +0530

    fixing import in firabse

--- backend/services/auth/config/firebase.js
+++ backend/services/auth/config/firebase.js
@@ -1,6 +1,10 @@
 import { cert, initializeApp } from "firebase-admin";
 
-import serviceAccount from "../serviceAccountKey.json" with { type: "json" };
+const path = process.env.FIREBASE_KEY_PATH || "./serviceAccountKey.json";
+// import serviceAccount from "../serviceAccountKey.json" with { type: "json" };
+const serviceAccount = await import(path, {
+  assert: { type: "json" }, // Node 18+ supports JSON modules
+});
 
 export const app = initializeApp({
   credential: cert(serviceAccount),
